42163c8f5816415ada359bb83ceecedb631d8a1f
Jinming Wu, Patrick <me@patrickwu.space>
2021-02-01 16:08:11 +0800

    first succesful build

--- build.ps1
+++ build.ps1
@@ -18,15 +18,21 @@ Invoke-WithInstance sudo sh get-docker.sh
 Invoke-WithInstance sudo usermod -aG docker $env:USERNAME
 wsl.exe -t ubuntu-$build_instance
 
-Invoke-WithInstance git clone https://github.com/microsoft/CBL-Mariner.git /home/$env:USERNAME/cblm
+Invoke-WithInstance git clone https://github.com/microsoft/CBL-Mariner.git ~/cblm
 Invoke-WithInstance git --work-tree=/home/$env:USERNAME/cblm checkout 1.0-stable
 
-Invoke-WithInstance sudo make `-C ~/cblm/toolkit/ toolchain REBUILD_TOOLS=y
-Invoke-WithInstance sudo make `-C ~/cblm/toolkit/ build`-packages `-j`$`(nproc`) CONFIG_FILE=./imageconfigs/core-container.json REBUILD_TOOLS=y REBUILD_PACKAGES=n PACKAGE_IGNORE_LIST='openjdk8 openjdk8_aarch64 shim-unsigned-aarch64'
-Invoke-WithInstance sudo make `-C ~/cblm/toolkit/ image CONFIG_FILE=./imageconfigs/core-container.json REBUILD_TOOLS=y DOWNLOAD_SRPMS=y
+Invoke-WithInstance bash -c `"sudo make `-C ~/cblm/toolkit/ image REBUILD_TOOLS=y REBUILD_PACKAGES=n CONFIG_FILE=~/cblm/toolkit/imageconfigs/core-container.json`"
+# for some unknown reason, first run will not always run successfully
+while (-not $?) {
+    Invoke-WithInstance bash -c `"sudo make `-C ~/cblm/toolkit/ image REBUILD_TOOLS=y REBUILD_PACKAGES=n CONFIG_FILE=~/cblm/toolkit/imageconfigs/core-container.json`"
+}
+#Invoke-WithInstance sudo make `-C ~/cblm/toolkit/ toolchain REBUILD_TOOLS=y REBUILD_TOOLCHAIN=y DOWNLOAD_SRPMS=y 
+#Invoke-WithInstance sudo make `-C ~/cblm/toolkit/ build`-packages `-j`$`(nproc`) CONFIG_FILE= REBUILD_TOOLS=y REBUILD_PACKAGES=n PACKAGE_IGNORE_LIST='openjdk8 openjdk8_aarch64 shim-unsigned-aarch64'
+#Invoke-WithInstance sudo make `-C ~/cblm/toolkit/ image CONFIG_FILE=./imageconfigs/core-container.json REBUILD_TOOLS=y DOWNLOAD_SRPMS=y
 
-Invoke-WithInstance cp `-r /home/$env:USERNAME/cblm/out/images/core-container/*.tar.gz .
+Invoke-WithInstance cp `-r ~/cblm/out/images/core-container/*.tar.gz .
 Move-Item *.tar.gz install.tar.gz
 wsl.exe --import $ImportName $ImportPath ./install.tar.gz --version $ImportVersion
 Remove-Item get-docker.sh
+Remove-Item install.tar.gz
 Remove-UbuntuWSLInstance -Id $build_instance
